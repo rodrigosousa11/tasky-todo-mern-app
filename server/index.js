a257c60705973d4e3e4588cfc9e79a40a31edfdb
Rodrigo <96240973+rodrigosousa11@users.noreply.github.com>
2023-08-02 15:47:05 +0100

    test

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const todoRoutes = require("./routes/todos");
@@ -20,6 +21,11 @@ mongoose
 
 app.use("/todos", todoRoutes);
 app.use("/users", userRoutes);
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 app.listen(process.env.PORT, () =>
 	console.log("Server started on port " + process.env.PORT)
