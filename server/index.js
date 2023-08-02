2370a48ceb082f793aabef530d596f6c5be85c4d
Rodrigo <96240973+rodrigosousa11@users.noreply.github.com>
2023-08-02 15:03:04 +0100

    deploy config changes

--- server/index.js
+++ server/index.js
@@ -21,4 +21,6 @@ mongoose
 app.use("/todos", todoRoutes);
 app.use("/users", userRoutes);
 
-app.listen(3000, () => console.log("Server started on port 3000"));
+app.listen(process.env.PORT, () =>
+	console.log("Server started on port " + process.env.PORT)
+);
